cc6f31ce4e3b23ba6c106ef004d08a0abfaacc0a
hylobates-lar <alison.v.quaglia@gmail.com>
2020-09-26 18:30:38 -0400

    Updates

---
+++ precache-manifest.a59952de3e5372dbf8c5db4fb729c2f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e902b85231e8f3ca81fdc8fe65e7ed4",
+    "url": "/tech-challenge/index.html"
+  },
+  {
+    "revision": "adbcd620677efbe973c0",
+    "url": "/tech-challenge/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "1a822dcd921351a61d0b",
+    "url": "/tech-challenge/static/css/main.62215959.chunk.css"
+  },
+  {
+    "revision": "adbcd620677efbe973c0",
+    "url": "/tech-challenge/static/js/2.8056e096.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tech-challenge/static/js/2.8056e096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a822dcd921351a61d0b",
+    "url": "/tech-challenge/static/js/main.fad80487.chunk.js"
+  },
+  {
+    "revision": "0191a981853fe2cd7013",
+    "url": "/tech-challenge/static/js/runtime-main.8d921d60.js"
+  }
+]);
